e8600f3484e612d89e102f4ffec04862f6954553
Igor <terehovi303@gmail.com>
2023-03-15 19:14:44 +0300

    Написал метод для генерации результирующего массива

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] GenrateResultArray(string[] start)
+{
+    int size = 0;
+    for (int i = 0; i < start.Length; i++)
+    {
+        if (start[i].Length <= 3)
+        {
+            size++;
+        }
+    }
+    string[] result = new string[size];
+    return result;
+}
