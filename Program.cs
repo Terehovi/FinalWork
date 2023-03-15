1a0e6b3a06c9760247cf806e07595c0c2d4427ee
Igor <terehovi303@gmail.com>
2023-03-15 19:16:09 +0300

    Написал метод для перезаписи нужных по условию элементов

--- Program.cs
+++ Program.cs
@@ -11,3 +11,20 @@
     string[] result = new string[size];
     return result;
 }
+void AddTheNecessaryElements(string[] start, string[] res)
+{
+    int n = 0;
+    int j = 0;
+    while (n < res.Length)
+    {
+        while (j < start.Length)
+        {
+            if (start[j].Length <= 3)
+            {
+                res[n] = start[j];
+                n++;
+            }
+            j++;
+        }
+    }
+}
